e5de13c5194a522acd5b227ccad43979938af37f
shalabyelectronics <mhdshalabystar@gmail.com>
2022-05-30 07:35:51 +0300

    edit BlogPost body text length

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ class BlogPost(db.Model):
     title = db.Column(db.String(250), unique=True, nullable=False)
     subtitle = db.Column(db.String(250), nullable=False)
     date = db.Column(db.String(250), nullable=False)
-    body = db.Column(db.Text(250), nullable=False)
+    body = db.Column(db.Text, nullable=False)
     img_url = db.Column(db.String(250), nullable=False)
     comments = relationship("Comment", back_populates="parent_post")
 
